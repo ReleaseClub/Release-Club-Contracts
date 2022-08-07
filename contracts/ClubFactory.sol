ea73611cf0c9af3cf2f909e9672b16772ce7efe1
JR <jose.alonso.programmer@gmail.com>
2022-08-07 19:23:06 +0200

    Format contract code and renamed deploy script

--- contracts/ClubFactory.sol
+++ contracts/ClubFactory.sol
@@ -4,22 +4,19 @@ pragma solidity ^0.8.9;
 import "contracts/ReleaseClub.sol";
 
 contract ClubFactory is Ownable {
-
     event ClubCreated(address ClubAddress, string clubName);
 
     address[] public clubs;
-    mapping(address=>address[]) public clubOwners;
+    mapping(address => address[]) public clubOwners;
 
     function addClub(string memory name) public payable {
-        ReleaseClub club = new ReleaseClub(name,msg.sender);
+        ReleaseClub club = new ReleaseClub(name, msg.sender);
         clubs.push(address(club));
         clubOwners[msg.sender].push(address(club));
-        emit ClubCreated(address(club),name);
-        
-        
+        emit ClubCreated(address(club), name);
     }
 
-    function viewClubs() public view returns(address[] memory){
+    function viewClubs() public view returns (address[] memory) {
         return clubs;
     }
 
