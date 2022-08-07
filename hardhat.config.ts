891cfe5fbe258157dd653d6bdbcfff0fd2c972cf
JR <jose.alonso.programmer@gmail.com>
2022-08-07 13:59:42 +0200

    Create .env file so the private key is not shown.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      chainId: 1337   // We set 1337 to make interacting with MetaMask simpler
+    },
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/gX8wdpwb8rjeJ7rG2ohv99REWOkts-mR", //Infura url with projectId
-      accounts: [""] // add the account that will deploy the contract (private key)
-     }
+      url: process.env.RINKEBY_API_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }
+  }
 };
 
 export default config;
