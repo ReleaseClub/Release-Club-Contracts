3df0fde4aafc05af3c6527a38a79decfbbc95c96
JR <jose.alonso.programmer@gmail.com>
2022-08-10 02:51:18 +0200

    Write some NatSpec documentation

--- contracts/ReleaseClub.sol
+++ contracts/ReleaseClub.sol
@@ -4,36 +4,54 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 struct Release {
     address tokenContract;
     uint256 tokenID;
 }
 
-contract ReleaseClub is AccessControlEnumerable{
-   
-   event NewRelease(address tokenContract,uint256 tokenID);
-   Release[] public releases;
-   bytes32 public constant MEMBER_ROLE = keccak256("MEMBER_ROLE");
-   bytes32 public constant MOD_ROLE = keccak256("MOD_ROLE");
-   string public clubName;
-    constructor(string memory name,address creator) {
+/**
+ * @dev The contract inherits the {AccessControlEnumerable} contract.
+ */
+contract ReleaseClub is AccessControlEnumerable {
+    /**
+     * @dev Emitted when there is a new release
+     */
+    event NewRelease(address tokenContract, uint256 tokenID);
+
+    /******************************* State variables ******************************/
+    Release[] public releases;
+    bytes32 public constant MEMBER_ROLE = keccak256("MEMBER_ROLE");
+    bytes32 public constant MOD_ROLE = keccak256("MOD_ROLE");
+    string public clubName;
+
+    constructor(string memory name, address creator) {
         // Grant the contract deployer the default admin role: it will be able
         // to grant and revoke any roles
-        clubName=name;
+        clubName = name;
         _setupRole(DEFAULT_ADMIN_ROLE, creator);
         _setupRole(MOD_ROLE, creator);
         _setupRole(MEMBER_ROLE, creator);
     }
 
+    /**
+     * @dev Returns the content of the `releases` array.
+     */
     function viewReleases() public view returns (Release[] memory) {
         return releases;
     }
 
+    /**
+     * @dev Returns the club name.
+     */
     function viewName() public view returns (string memory) {
         return clubName;
     }
 
+    /**
+     * @dev Returns the a list with all the members addresses.
+     */
     function viewMembers() public view returns (address[] memory) {
         uint256 length = getRoleMemberCount(MEMBER_ROLE);
         address[] memory addresses = new address[](length);
@@ -68,14 +86,20 @@ contract ReleaseClub is AccessControlEnumerable{
     {
         _revokeRole(MOD_ROLE, account);
     }
-    
-   function addRelease(Release[] memory newReleases) public onlyRole (MEMBER_ROLE) {
-       uint256 i = 0;
-       while(i<newReleases.length)
-       {
-           releases.push(newReleases[i]);
-           emit NewRelease(newReleases[i].tokenContract,newReleases[i].tokenID);
-           i++;
-       }
-   }
+
+    function addRelease(Release[] memory newReleases)
+        public
+        onlyRole(MEMBER_ROLE)
+    {
+        uint256 i = 0;
+        while (i < newReleases.length) {
+            releases.push(newReleases[i]);
+            emit NewRelease(
+                newReleases[i].tokenContract,
+                newReleases[i].tokenID
+            );
+            i++;
+        }
+    }
+
 }
